799aa443e2a265c80c5b51824385a92887d45302
Thiago Moreira <thiago.cmoreira@yahoo.com.br>
2019-04-19 22:14:43 -0300

    Creating header component

--- src/layouts/TopBarLayout.vue
+++ src/layouts/TopBarLayout.vue
@@ -1,24 +1,16 @@
 <template lang="pug">
   q-layout(view="hHh lpr fFf")
-    q-header(reveal).bg-primary.text-white.q-pa-sm.shadow-global
-      q-toolbar
-        q-toolbar-title
-          q-avatar
-            img(src="https://cdn.quasar-framework.org/logo/svg/quasar-logo.svg")
-
+    page-header
     q-page-container
       router-view
-
-    // q-footer(reveal).bg-grey-8.text-white
-    //   q-toolbar
-    //     q-toolbar-title Title
 </template>
 
 <script>
+import PageHeader from '../components/PageHeader'
 export default {
-  name: 'TopBarLayout'
+  name: 'TopBarLayout',
+  components: {
+    PageHeader
+  }
 }
 </script>
-
-<style>
-</style>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,10 +1,12 @@
 <template lang="pug">
   q-page.index.column.items-center.bg-secondary
-    div.index-container.full-width.q-py-md
+    div.index-container.full-width.q-py-lg
       div.cards.flex.full-width.global-shadow
         div(v-for="c of cards").card.q-pa-md.q-pb-lg.bg-primary.flex-1.q-ma-sm.shadow-global
           h4.text-grey-3 {{ c.title }}
           h1(:class="{ 'text-positive': c.positive }").text-bold {{ c.value }}
+      div.card.full-width.q-mt-lg.bg-primary.text-white.shadow-global.q-pa-md
+        h2 Monitoramentos Anteriores
 </template>
 
 <style>
@@ -47,21 +49,34 @@ export default {
 
 .index-container
   max-width 950px
-  height calc(100vh - 66px)
 
 .card
   border-radius 5px
+
+  &:first-of-type
+    margin-left 0
+
+  &:last-of-type
+    margin-right 0
 
   h4
     margin 0 0 15px 0
     text-align left
     font-size 16px
     line-height 20px
+    font-weight 500
 
   h1
     margin 0
-    font-size 34px
-    line-height 34px
+    font-size 38px
+    line-height 38px
     text-align center
     color white
+
+  h2
+    margin 0 0 15px 0
+    text-align left
+    font-size 24px
+    line-height 24px
+    font-weight 500
 </style>
