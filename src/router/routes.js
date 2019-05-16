92f5fc01fe726461ddbdfee4330c035ac415216b
Thiago Moreira <thiago.cmoreira@yahoo.com.br>
2019-05-16 02:07:01 -0300

    General adjusts and adding gql file and route to cables

--- src/router/routes.js
+++ src/router/routes.js
@@ -6,6 +6,13 @@ const routes = [
     children: [
       { path: '', component: () => import('pages/Index.vue') }
     ]
+  },
+  {
+    path: '/cables',
+    component: () => import('layouts/TopBarLayout.vue'),
+    children: [
+      { path: '', component: () => import('pages/CablePage.vue') }
+    ]
   }
 ]
 
