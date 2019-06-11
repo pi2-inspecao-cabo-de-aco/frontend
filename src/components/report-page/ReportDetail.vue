181d6aa25de1b2e8a855666d06f345a2e4cbc315
Lucas Andrade <lucasandrad@yandex.com>
2019-06-10 22:11:52 -0300

    Start to create the layout for Report Detail page #35

---
+++ src/components/report-page/ReportDetail.vue
@@ -0,0 +1,49 @@
+<template lang='pug'>
+  div.q-pa-lg
+    div.full-width.column.flex-1.justify-center.items-center
+      div.report-detail.col-auto
+        q-card
+          q-card-section
+            div.text-h6 Informações
+          div.attribute.flex
+            div.label Início:
+            div.value {{ '10/10/2018' }}
+          q-separator
+          div.attribute.flex
+            div.label Fim:
+            div.value {{ '10/10/2018' }}
+          q-separator
+          div.attribute.flex
+            div.label Nível de Alerta:
+            div.value {{ 'High' }}
+
+</template>
+
+<script>
+export default {
+  name: 'ReportDetail',
+  props: {
+    report: {
+      type: {
+        start: '09/06/2019',
+        end: '09/06/2019',
+        alert_level: 'High'
+      }
+    }
+  }
+}
+</script>
+
+<style lang='stylus' scoped>
+.report-detail
+  width 80%
+  padding 15px
+  background #ffffff
+  .attribute
+    padding 10px 15px
+    margin 5px 10px
+    border-radius 10px
+    .label
+      font-size 100%
+      margin-right 10px
+</style>
---
+++ src/pages/ShowReportPage.vue
@@ -0,0 +1,43 @@
+<template lang="pug">
+  report-detail
+</template>
+
+<script>
+import REPORT from '../graphql/queries/report.gql'
+import { mapGetters, mapActions } from 'vuex'
+
+export default {
+  name: 'ShowReportPage',
+  components: {
+    ReportDetail: () => import('../components/report-page/ReportDetail')
+  },
+  apollo: {
+    report () {
+      return {
+        query: REPORT,
+        variables: {
+          reportId: this.currentReportId
+        },
+        fetchPolicy: 'network-only',
+        update (data) {
+          return data.report
+        }
+      }
+    }
+  },
+  data () {
+    return {
+      report: []
+    }
+  },
+  computed: {
+    ...mapGetters('reports', [
+      'currentReportId'
+    ])
+  }
+}
+</script>
+
+<style>
+
+</style>
